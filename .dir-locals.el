f5e8f066600a9b55e2fd220f09517390654c9ea7
David Vaz <davidmgvaz@gmail.com>
2012-08-02 16:48:45 +0100

    emacs compile customization

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((eval . 
+               (setq default-directory (locate-dominating-file
+                                        buffer-file-name ".dir-locals.el")
+                     ))))
+ (c-mode . ((compile-command . "cd arch && make install")))
+)
